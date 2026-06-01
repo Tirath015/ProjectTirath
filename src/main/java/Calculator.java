b8a7d958b9eb6b0c3e444158278d747d96cdae57
Tirath Singh <paddati@sheridancollege.ca>
2026-06-01 12:49:01 -0400

    Initial Commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author  Tirath Singh Padda, 2026
+
+ */
+public class Calculator {
+    
+    
+
+}
---
+++ src/main/java/com/mycompany/ice_04/Ice_04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice_04;
+
+/**
+ *
+ * @author Tirath Singh Padda , 2026
+ */
+public class Ice_04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
